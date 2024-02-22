2cba44aa3ae2b44c960b726f535da4fb777b1548
Michel Dellabeneta <85519685+dellabeneta@users.noreply.github.com>
2024-02-22 03:12:25 -0300

    Update helm.tf

--- modules/loadbalancer/helm.tf
+++ modules/loadbalancer/helm.tf
@@ -2,7 +2,7 @@ resource "helm_release" "eks_helm_controller" {
   name       = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
-  //version    = "1.7.1"
+  version    = "1.7.1"
   namespace  = "kube-system"
 
   set {
