bfea4f40ca175802b4b8ca73689648194e7175b4
dellabeneta <m.dellabeneta@gmail.com>
2024-02-22 02:53:43 -0300

    Ajustes de versão do Helm Chart e Instances Types

--- modules/loadbalancer/serviceaccount.tf
+++ modules/loadbalancer/serviceaccount.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_service_account" "eks_controller_sa" {
   metadata {
-    name      = "load-balancer-controller"
+    name      = "aws-load-balancer-controller"
     namespace = "kube-system"
     annotations = {
       "eks.amazonaws.com/role-arn" = aws_iam_role.eks_controller_role.arn
